6fa49df6585c5505e730469351e2a4489e654b2b
John Beisley <githubpublic.abide102@passmail.net>
2026-06-21 14:39:13 +0100

    add devenv for system dependencies

---
+++ devenv.nix
@@ -0,0 +1,28 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/packages/
+  packages = with pkgs; [
+    git
+    poppler-utils  # Requirement for running.
+  ];
+
+  # https://devenv.sh/languages/
+  languages.python = {
+    enable = true;
+    version = "3.14";
+    venv.enable = true;
+    uv = {
+      enable = true;
+      sync.enable = true;
+    };
+  };
+
+  # https://devenv.sh/basics/
+  enterShell = '''';
+
+  # https://devenv.sh/git-hooks/
+  # git-hooks.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
